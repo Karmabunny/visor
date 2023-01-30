ea0dd70fad71be5998d360d0b80dd9aa0210d056
Gwilyn <gwilyn@karmabunny.com.au>
2023-01-30 10:30:12 +1030

    Roll-up CLI request builder into a class file.

---
+++ src/CliRequest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ *
+ * @package karmabunny/echoserver
+ */
+class CliRequest
+{
+    /** @var string */
+    public $path;
+
+    /** @var string[]|array[] [key => value] */
+    public $query;
+
+    /** @var string */
+    public $method;
+
+    /** @var string[] [key => value] */
+    public $headers;
+
+    /** @var string */
+    public $body;
+
+
+    protected function __construct(array $config)
+    {
+        foreach ($config as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+
+    public static function create()
+    {
+        // Path.
+        [$path, $query_string] = explode('?', $_SERVER['REQUEST_URI'], 2) + [null, null];
+
+        // Method.
+        $method = strtoupper(@$_SERVER['REQUEST_METHOD'] ?: 'GET');
+
+        // Query strings.
+        $query = null;
+        parse_str($query_string, $query);
+
+        // Parse headers.
+        $headers = [];
+
+        foreach ($_SERVER as $key => $value) {
+            $key = strtolower($key);
+
+            if (strpos($key, 'http_') === 0) {
+                $key = str_replace('_', '-', substr($key, 5));
+            }
+            else if (strpos($key, 'content_') === 0) {
+                $key = str_replace('_', '-', $key);
+            }
+            else {
+                continue;
+            }
+
+            $headers[$key] = $value;
+        }
+
+        // Raw bodies.
+        // Doesn't support multipart/form-data.
+        $body = file_get_contents('php://input');
+        if ($body === false) {
+            $body = null;
+        }
+
+        return new static(compact('path', 'query', 'method', 'headers', 'body'));
+    }
+
+
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+
+}
--- src/echo.php
+++ src/echo.php
@@ -7,49 +7,15 @@ if (
     PHP_SAPI === 'cli-server'
     and get_included_files()[0] === __FILE__
 ) {
-    // Path.
-    [$path, $query_string] = explode('?', $_SERVER['REQUEST_URI'], 2) + [null, null];
+    require __DIR__ . '/CliRequest.php';
+    $request = CliRequest::create();
 
-    // Method.
-    $method = strtoupper(@$_SERVER['REQUEST_METHOD'] ?: 'GET');
+    error_log("[{$request->method}] {$request->path}");
 
-    // Query strings.
-    $query = null;
-    parse_str($query_string, $query);
+    $payload = $request->toArray();
+    $payload = json_encode($payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
-    // JSON bodies.
-    $body = file_get_contents('php://input');
-    $body = json_decode($body, true);
-
-    // Parse headers.
-    $headers = [];
-
-    foreach ($_SERVER as $key => $value) {
-        $key = strtolower($key);
-
-        if (strpos($key, 'http_') === 0) {
-            $key = str_replace('_', '-', substr($key, 5));
-        }
-        else if (strpos($key, 'content_') === 0) {
-            $key = str_replace('_', '-', $key);
-        }
-        else {
-            continue;
-        }
-
-        $headers[$key] = $value;
-    }
-
-    // Dump it.
-    $payload = json_encode([
-        'headers' => $headers,
-        'path' => $path,
-        'query' => $query,
-        'method' => $method,
-        'body' => $body,
-    ], JSON_PRETTY_PRINT);
-
-    header('content-type: application/json');
+    header('Content-Type: application/json');
     echo $payload;
 
     file_put_contents(getcwd() . '/latest.json', $payload);
